2c599d93a35c533780d358275605376740e66034
nramiscal <nramiscal@gmail.com>
2017-11-28 01:12:07 -0500

    fixed comments

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var bodyParser = require('body-parser');
 var path = require('path');
 var port = 8000;
 
-mongoose.connect('mongodb://localhost/basic_validation'); // connecting to db 'basic_mongoose'
-mongoose.promise = global.Promise; // use native promises
+mongoose.connect('mongodb://localhost/basic_validation'); // creates db 'basic_validation' if doesn't exist
+mongoose.promise = global.Promise;
 
 var UserSchema = new mongoose.Schema({
     firstName:  { type: String, required: true, minlength: 1},
@@ -14,8 +14,8 @@ var UserSchema = new mongoose.Schema({
     age: { type: Number, required: true, min: 1, max: 150 },
     email: { type: String, required: true }
 }, {timestamps: true });
-mongoose.model('User', UserSchema); // setting this schema in our Models as 'User'
-var User = mongoose.model('User'); // retrieving this schema from our Models, named 'User'
+mongoose.model('User', UserSchema);
+var User = mongoose.model('User');
 
 app.use(express.static(path.join(__dirname, './static')));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -32,9 +32,8 @@ app.get('/', function(req, res){
 
 app.post('/users', function(req, res){
     console.log("POST DATA", req.body);
-    // code to add user to db
     var user = new User(req.body);
-    user.save(function(err){
+    user.save(function(err){ // save user to database
         if (err){
             var users = User.find();
             res.render('index', {errors: user.errors, users: users});
